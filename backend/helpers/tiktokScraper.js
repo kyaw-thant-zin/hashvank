6717f5d5981d5542ab485c8c9e2d8d7b69cb5eb3
Kyaw Thant Zin <kyawthantzamt@gmail.com>
2022-10-12 10:51:52 +0630

    Update tiktokScraper.js

--- backend/helpers/tiktokScraper.js
+++ backend/helpers/tiktokScraper.js
@@ -4,7 +4,8 @@
 const axios = require('axios')
 
 const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36'
-const profileURL = 'https://www.tiktok.com/node/share/user/@'
+// const profileURL = 'https://www.tiktok.com/node/share/user/@'
+const profileURL = 'https://www.tiktok.com/api/user/detail/'
 const tagURL = 'https://www.tiktok.com/node/share/tag/'
 const challengeURL = 'https://m.tiktok.com/api/challenge/item_list/'
 const searchURL = 'https://www.tiktok.com/api/search/general/full/'
@@ -43,14 +44,43 @@ const baseParams = {
     "webcast_language": "en",
 }
 
-const cookies = [
-    
-]
+const profileParams = {
+    'aid': '1988',
+    "app_language": "en",
+    "app_name": "tiktok_web",
+    "battery_info": 1,
+    "browser_language": "en",
+    "browser_name": "Mozilla",
+    "browser_online": true,
+    "browser_platform": "MacIntel",
+    "browser_version": '5.0(Macintosh; Intel Mac OS X 10 _15_7) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 105.0 .0 .0 Safari / 537.36',
+    "channel": "tiktok_web",
+    "cookie_enabled": true,
+    "device_id": "7153125753357747714",
+    "device_platform": "web_pc",
+    "focus_state": true,
+    "from_page": "user",
+    "history_len": 14,
+    "is_fullscreen": false,
+    "is_page_visible": true,
+    "language": "en",
+    "os": "mac",
+    "priority_region": "",
+    "referer": "",
+    "region": "MM",
+    "screen_height": "1080",
+    "screen_width": "1920",
+    "tz_name": "Asia/Rangoon",
+    "webcast_language": "en",
+    "msToken": "1BryaEhw6wUzEjGIqicKSX8SwmQmibTCUCd4abZTwaHNaOVKSWBlAlsU1rKeNOFKnwdcOeU0XuQULYJKEhYj7Nt7t64xDHqid5hZShL3R0ziTBWl6NCa6mBB0GjN1u4Vvc-BS8s=",
+}
+
+const cookies = 'ttwid=1|SKruGNyl0VDzXp7EMZoTFhX1V3bsfQ4oVk2P56ZU3tw|1664249066|6a88a3427d163859bce5a6b7861dbfa496c9fdd04f5833efdb98afe355785537; bm_sv=AB306D277837678D3316833FBACA732B~YAAQnKg7F+O8nFmDAQAALHP5fBGB5KkEAoIc4tRC4KpUmJmEKGD3qUvl0j3aTw5WDw26350XITc21EWDWai7Mu/8zO2w4J/1M0HBCdH7fId8eq/2CVIbIeTC0C7ufPVZMOEwWf4nWSS5bpKYaA+kCW1mIfra+vJOL6P6Lyu2m643gnEGbgpEmLEytRubzM8DlDeixTVWo4vWjnRPwjeVJjIBlKSU+a+ATW2zlQmi8UJDu1+2oF948WXdrH3piLaJ~1; msToken=Z5hEAk6LKoFW1rGvoEfLCMAVeBW_i2uJOPSqWW4yeD83_Cv7p3q613uaQpA1rOVV-Rhj0oZwHvt8KQhqY_kQbarAUJZLC45heWXtbv0ShKxNc_UWq7VgnMuJ7cxBcaGFwRJJJrU=; msToken=Z5hEAk6LKoFW1rGvoEfLCMAVeBW_i2uJOPSqWW4yeD83_Cv7p3q613uaQpA1rOVV-Rhj0oZwHvt8KQhqY_kQbarAUJZLC45heWXtbv0ShKxNc_UWq7VgnMuJ7cxBcaGFwRJJJrU='
 
 const headers = {
     'user-agent': USER_AGENT,
     'referer': 'https://www.tiktok.com/',
-    'Cookie': 'tt_csrf_token=oVjx5ujE-m0pf3rkaWsi2I9ptPBVU3Wq3CkU; cookie-consent={"ga":true,"af":true,"fbp":true,"lip":true,"bing":true,"ttads":true,"reddit":true,"version":"v8"}; tiktok_webapp_theme=dark; __tea_cache_tokens_1988={"user_unique_id":"7146454663906182657","timestamp":1663913651936,"_type_":"default"}; _ttp=2F9rg7gKJtY98sLQtHXaLf37M0T; _abck=8D5A396F3103ADF2CD3722EAF31A30F3~-1~YAAQnKg7F62ynFmDAQAAhlX3fAjCHf36B7dPxJqsrVdh/p+UfXZMMADQc+M3WyQveDcDFtlnhZ3xUzRDox3KvqCHO04Rsi5BoeAKcI9+Qvial6gCFKFl88SmkbadUv9lzP6ds22XbaL2XdBWxIBazjAK5KoJVbMcdjcATHt5f6RdBqq7uHYu1jB5gOlf4WYj/g35zkk6hpuMN1Qh9SKGg/5SV5ldgdb8ysjGMvIK7IeYvW+OGaqCy4QDMgt+XsOHLw/zcaIYPW8EmqGIZT32op+S7Jf8oiQ1j+lQZWR98WCr7DC82jp80cXQ3DtgrOhnnktq4FyH+TFii9Y3ZclEAs8j4LIdxiSCBmjvAR6UWmBhCpQrJ1JxaHpj33fiIyxanow0nvTkFg==~-1~-1~-1; ak_bmsc=50AFB1B22E2CFA59451F013FB783C6DB~000000000000000000000000000000~YAAQnKg7F66ynFmDAQAAhlX3fBGiW6R2W96/sLT8HpDaCr+DeMyIvZZwC0q/AcccaRizC1k9k/zSbq7VP8qlirHFSpgCHfeV4fpFUw+XohD41exw5ERVn9jvslbsOgxzplO5blC2lpdyJyydKhD2D1sFEXmgXZRBSEQhA03h9IZtFRwBcHnQBNpwo4IVpTsV6Rcl3aCDFwQCFGeWPlaTIVb2gTUHIMiP3mcbApRuVZ15ssedZZSK2behqWeANBAbmryVsfb1+s/egROkK8W3yQ7WB09pabisscdlA4f33wb2U2cvuxizfvJbO2RwHLbZPr29mBJubIXRWBsbSlCEvM0+4YLMeomRIGVKp+jbSEX40xJyHbB+FTV2cd67v8qx2oOAQx8iyebV; bm_sz=C94F6C2D7025285F0AE128F9D0C00ACB~YAAQnKg7F7CynFmDAQAAhlX3fBEfCe12pZ1PF6MBbdGthopmp/9PvtSUiI6flufVd7Ek9dJMjh8jW1tx6c2djlOFcFbDertdsYSwnref55WAUDyqyEYD8978lrjZhtivXk+wANC0u4BYvMyTDFIPJusJf+BSGvPKfTv0dQbO8VxtsHzZKnWySyrC3fHnwoE9ImKVBR2MWkca6X+CJPadRUAeb0ehdilNqr1BwFcSCgEwioMkCV4QKsF5dzohVLgZGPHAbhzOD3mxRjtXjR915gX/cZO5MMsukV5bT4N7sYtnQ3M=~3425846~3553073; bm_mi=0703A966F5F87AD4A44BBCF56D923FB8~YAAQnKg7F8mynFmDAQAAe2D3fBEvuyMYm+QLvepZsNB4IoUQt/kVql0HeW4W6DrjjIYIJhr0Cj+hRHVAF+Kb5e4k2ykICVoH4nlAM9dgXa7jAHZG0+pbrqAgk20V8KOxV53R0OhPsiK7lLmvK3K6p2548v/fNNieKX5dB0goPH2sPtkKo48W/hMOKiCtoNA8NTlnzmXAk59Wh2x0rbVOTiIFGBMgNz/CE7ERLArf+5v9gOwFysPlEASijXJsetX96XjtwD+/yxB7sM3QYGSPxmoUdVgbvreSZ62T+w6OqONQYjbb2es48cQ293YR~1; ttwid=1|SKruGNyl0VDzXp7EMZoTFhX1V3bsfQ4oVk2P56ZU3tw|1664249066|6a88a3427d163859bce5a6b7861dbfa496c9fdd04f5833efdb98afe355785537; bm_sv=AB306D277837678D3316833FBACA732B~YAAQnKg7F+O8nFmDAQAALHP5fBGB5KkEAoIc4tRC4KpUmJmEKGD3qUvl0j3aTw5WDw26350XITc21EWDWai7Mu/8zO2w4J/1M0HBCdH7fId8eq/2CVIbIeTC0C7ufPVZMOEwWf4nWSS5bpKYaA+kCW1mIfra+vJOL6P6Lyu2m643gnEGbgpEmLEytRubzM8DlDeixTVWo4vWjnRPwjeVJjIBlKSU+a+ATW2zlQmi8UJDu1+2oF948WXdrH3piLaJ~1; msToken=Z5hEAk6LKoFW1rGvoEfLCMAVeBW_i2uJOPSqWW4yeD83_Cv7p3q613uaQpA1rOVV-Rhj0oZwHvt8KQhqY_kQbarAUJZLC45heWXtbv0ShKxNc_UWq7VgnMuJ7cxBcaGFwRJJJrU=; msToken=Z5hEAk6LKoFW1rGvoEfLCMAVeBW_i2uJOPSqWW4yeD83_Cv7p3q613uaQpA1rOVV-Rhj0oZwHvt8KQhqY_kQbarAUJZLC45heWXtbv0ShKxNc_UWq7VgnMuJ7cxBcaGFwRJJJrU='
+    'Cookie': cookies
 }
 
 const scrape = async (url, type) => {
@@ -63,6 +93,8 @@ const scrape = async (url, type) => {
             url: url,
             withCredentials: true,
         });
+
+        console.log(response.data)
 
         if (type === 'hashtag') {
             if('data' in response.data) {
@@ -91,7 +123,7 @@ const getResponse = async (data, type) => {
         resposne.collector = data.map((d) => {
 
             const tiktok = d.item
-    
+            console.log(tiktok)
             return {
                 id: tiktok.video.id,
                 text: tiktok.desc,
@@ -195,9 +227,12 @@ const getTikTokByAccount = async (account, options) => {
                 options.count = options.number
             }
 
-            const qsObject = new URLSearchParams(baseParams)
+            profileParams.uniqueId = account
+
+            const qsObject = new URLSearchParams(profileParams)
             const qs = qsObject.toString()
-            const url = profileURL + tiktokAcc + `?aid=1988`
+            const url = profileURL + `?` + qs
+
 
             const res = await axios({
                 method: 'get',
@@ -206,22 +241,20 @@ const getTikTokByAccount = async (account, options) => {
                 withCredentials: true,
             });
 
-            console.log(res.data)
+            let secUid = ''
+            if('userInfo' in res.data) {
+                secUid = res.data.userInfo.user.secUid
+            } else {
+                reject('error')
+            }
 
-            // let secUid = ''
-            // if('userInfo' in res.data) {
-            //     secUid = res.data.userInfo.user.secUid
-            // } else {
-            //     reject('error')
-            // }
+            baseParams.secUid = secUid
+            const qsObject2 = new URLSearchParams(baseParams)
+            const qs2 = qsObject2.toString()
+            const url2 = videoURL + `?${qs2}`
 
-            // baseParams.secUid = secUid
-            // const qsObject2 = new URLSearchParams(baseParams)
-            // const qs2 = qsObject2.toString()
-            // const url2 = videoURL + `?${qs2}`
-
-            // const response = await scrape(url2, 'account')
-            // resovle(response)
+            const response = await scrape(url2, 'account')
+            resovle(response)
 
         } catch (error) {
             console.log(error)
