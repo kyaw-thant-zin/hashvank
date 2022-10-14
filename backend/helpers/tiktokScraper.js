2264bdf67d7cd8375ef55ecab05bda8233cf5dae
Kyaw Thant Zin <kyawthantzamt@gmail.com>
2022-10-14 12:52:23 +0630

    Changing the scraping method
    First create a fake request and get cookies and call real fetch request.

--- backend/helpers/tiktokScraper.js
+++ backend/helpers/tiktokScraper.js
@@ -1,6 +1,6 @@
 
 // Scraping Tool
-
+const Signer = require("tiktok-signature")
 const axios = require('axios')
 const { wrapper } = require('axios-cookiejar-support')
 const { config } = require('dotenv')
@@ -12,10 +12,12 @@ const Cookie = db.cookies
 const Campaign = db.campaigns
 
 const jar = new CookieJar();
-const client = wrapper(axios.create({ jar }));
+const client = wrapper(axios.create({ jar }))
+const signer = new Signer()
 
-const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36'
+const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
 // const profileURL = 'https://www.tiktok.com/node/share/user/@'
+const searchProfileURL = 'https://www.tiktok.com/api/search/user/full/'
 const profileURL = 'https://www.tiktok.com/api/user/detail/'
 const tagURL = 'https://www.tiktok.com/node/share/tag/'
 const challengeURL = 'https://m.tiktok.com/api/challenge/item_list/'
@@ -97,7 +99,7 @@ const profileParams = {
     "browser_version": '5.0(Macintosh; Intel Mac OS X 10 _15_7) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 105.0 .0 .0 Safari / 537.36',
     "channel": "tiktok_web",
     "cookie_enabled": true,
-    "device_id": "7153125753357747714",
+    "device_id": "7154229846167668226",
     "device_platform": "web_pc",
     "focus_state": true,
     "from_page": "user",
@@ -113,18 +115,48 @@ const profileParams = {
     "screen_width": "1920",
     "tz_name": "Asia/Rangoon",
     "webcast_language": "en",
-    "msToken": "Cw11w5xngPhJtFu0saK5gZjMzSlJIikj31niibU8ApRHRrSNJCg0oJcMrYfXUCDpeL8LcAsLYawt1Zj_CwbvdK8kKSsRJPFUYsVVgS2IPeIi5bmMzkmqoIWO-wBL1lOQ0wT7_JA="
+    "secUid": '',
+    "msToken": "DYFrhlxnTlZzh0sbMv39Iq_B-OcAIBwqlXvZtisYcV7rRSbTGoub9nlhGkYv7_kEb_ubxzlhrN9uzJDQxmfx2Rth4x_nv56rtTqO3FCi9MhC8YrDid86YVmySyRVHxbo88AJL0M="
+}
+
+const secondParams = {
+    "app_language": "en",
+    "app_name": "tiktok_web",
+    "battery_info": 1,
+    "browser_language": "en",
+    "browser_name": "Mozilla",
+    "browser_online": true,
+    "browser_platform": "MacIntel",
+    "browser_version": '5.0(Macintosh; Intel Mac OS X 10 _15_7) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 105.0 .0 .0 Safari / 537.36',
+    "channel": "tiktok_web",
+    "cookie_enabled": true,
+    "device_id": "7154229846167668226",
+    "device_platform": "web_pc",
+    "focus_state": true,
+    "from_page": "user",
+    "history_len": 14,
+    "is_fullscreen": false,
+    "is_page_visible": true,
+    "language": "en",
+    "os": "mac",
+    "priority_region": "",
+    "referer": "",
+    "region": "MM",
+    "screen_height": "1080",
+    "screen_width": "1920",
+    "tz_name": "Asia/Rangoon",
+    "webcast_language": "en",
 }
 
 const cookies = {
-    "ttwid": "1|SKruGNyl0VDzXp7EMZoTFhX1V3bsfQ4oVk2P56ZU3tw|1664249066|6a88a3427d163859bce5a6b7861dbfa496c9fdd04f5833efdb98afe355785537; ",
-    "msToken": "Cw11w5xngPhJtFu0saK5gZjMzSlJIikj31niibU8ApRHRrSNJCg0oJcMrYfXUCDpeL8LcAsLYawt1Zj_CwbvdK8kKSsRJPFUYsVVgS2IPeIi5bmMzkmqoIWO-wBL1lOQ0wT7_JA="
+    "ttwid": "1%7CgGyKcS6dRMVu4XUfcXkjr3VAQip-BTv8U-wXGr0uDJI%7C1665723957%7C85238a14e13bbde6d8efc37e6f338a4bfac4a3e66ace1fbf043c05db68db1e77; ",
+    "msToken": "DYFrhlxnTlZzh0sbMv39Iq_B-OcAIBwqlXvZtisYcV7rRSbTGoub9nlhGkYv7_kEb_ubxzlhrN9uzJDQxmfx2Rth4x_nv56rtTqO3FCi9MhC8YrDid86YVmySyRVHxbo88AJL0M="
 }
 
 
 const headers = {
-    'user-agent': USER_AGENT,
-    'referer': 'https://www.tiktok.com/',
+    'User-Agent': USER_AGENT,
+    'Referer': 'https://www.tiktok.com/',
     'Cookie': objToString(cookies),
     'Content-Type': 'application/x-www-form-urlencoded',
     'Access-Control-Allow-Origin': '*',
@@ -308,25 +340,49 @@ const getTikTokByAccount = async (account, options) => {
                 }
             })
 
+            // ----------- Search and get secUid ---------- //
             // change the default value to fetch value
             profileParams.uniqueId = tiktokAcc    
             profileParams.msToken = cookie.msToken
-            cookies.msToken = cookie.msToken
+            cookie.msToken = cookie.msToken
 
             const qsObject = new URLSearchParams(profileParams)
             const qs = qsObject.toString()
             const url = profileURL + `?` + qs
 
+            await signer.init()
+            const signature = await signer.sign(url)
+
             const res = await client({
                 method: 'get',
                 headers: headers,
-                url: url,
+                url: signature.signed_url,
                 withCredentials: true,
             });
 
+            console.log('search.........')
+
+            console.log('fetch.........')
+            secondParams.uniqueId = tiktokAcc 
+
+            const qsObject2 = new URLSearchParams(secondParams)
+            const qs2 = qsObject2.toString()
+            const url2 = profileURL + `?` + qs2
+
+            const res2 = await client({
+                method: 'get',
+                headers: headers,
+                url: url2,
+                withCredentials: true,
+            });
+
+            console.log(res2.data.userInfo)
+
+
+
             // generate the msToken 
-            if(res.config?.jar?.toJSON()) {
-                const responseCookies = res.config.jar.toJSON().cookies;
+            if(res2.config?.jar?.toJSON()) {
+                const responseCookies = res2.config.jar.toJSON().cookies;
                 const msToken = responseCookies.filter((c)=> {
                     return c.key === 'msToken';
                 }).map(function(obj) {
@@ -345,35 +401,103 @@ const getTikTokByAccount = async (account, options) => {
                 })
             }
 
+            
+
+            // ----------- Search and get secUid ---------- //
+
+            // ----------- get user with secUid ---------- //
+            // if(res.data?.user_list) {
+
+            //     const sec_uid = res.data.user_list.filter((u) => {
+            //         return u.user_info?.unique_id === tiktokAcc
+            //     }).map((u) => {
+            //         return u?.user_info?.sec_uid
+            //     })
+
+            //     if(sec_uid.length > 0) {
+
+            //         const cookie = await Cookie.findOne({ 
+            //             where: {
+            //                 id: 1
+            //             }
+            //         })
+
+            //         profileParams.uniqueId = tiktokAcc    
+            //         profileParams.msToken = cookie.msToken
+            //         profileParams.secUid = sec_uid[0]
+            //         cookies.msToken = cookie.msToken
+
+            //         const qsObject = new URLSearchParams(profileParams)
+            //         const qs = qsObject.toString()
+            //         const url = profileURL + `?` + qs
+
+            //         const signature = await signer.sign(url)
+
+            //         const resUser = await client({
+            //             method: 'get',
+            //             headers: headers,
+            //             url: signature.signed_url,
+            //             withCredentials: true,
+            //         });
+
+            //         console.log('fetch.........')
+
+            //         console.log(resUser)
+
+            //     }
+
+            // }
+
+            // ----------- get user with secUid ---------- //
+
+            // generate the msToken 
+            // if(res.config?.jar?.toJSON()) {
+            //     const responseCookies = res.config.jar.toJSON().cookies;
+            //     const msToken = responseCookies.filter((c)=> {
+            //         return c.key === 'msToken';
+            //     }).map(function(obj) {
+            //         return obj.value;
+            //     });
+
+            //     const cookieData = {
+            //         "msToken": msToken[0]
+            //     }
+
+            //     // update the msToken for next requests
+            //     await Cookie.update( cookieData, {
+            //         where: {
+            //             id: 1,
+            //         }
+            //     })
+            // }
+
             let secUid = ''
             let videoCount = 0
 
-            console.log(res.data)
+            // if( res.data != '' && 'userInfo' in res.data) {
+            //     secUid = res.data.userInfo.user.secUid
+            //     videoCount = res.data.userInfo.stats.videoCount
 
-            if( res.data != '' && 'userInfo' in res.data) {
-                secUid = res.data.userInfo.user.secUid
-                videoCount = res.data.userInfo.stats.videoCount
+            //     baseParamsProfile.secUid = secUid
+            //     baseParamsProfile.count = options.count
+            //     baseParamsProfile.cursor = options.cursor
 
-                baseParamsProfile.secUid = secUid
-                baseParamsProfile.count = options.count
-                baseParamsProfile.cursor = options.cursor
+            //     const qsObject2 = new URLSearchParams(baseParamsProfile)
+            //     const qs2 = qsObject2.toString()
+            //     const url2 = videoURL + `?${qs2}`
 
-                const qsObject2 = new URLSearchParams(baseParamsProfile)
-                const qs2 = qsObject2.toString()
-                const url2 = videoURL + `?${qs2}`
+            //     const scrapeOptions = {
+            //         type: 'account',
+            //         offset: options.offset,
+            //         videoCount: videoCount
+            //     }
+            //     const response = await scrape(url2, scrapeOptions)
 
-                const scrapeOptions = {
-                    type: 'account',
-                    offset: options.offset,
-                    videoCount: videoCount
-                }
-                const response = await scrape(url2, scrapeOptions)
+            //     resovle(response)
 
-                resovle(response)
-
-            } else {
-                resovle('')
-            }
+            // } else {
+            //     resovle('')
+            // }
         } catch (error) {
             console.log(error)
         }
